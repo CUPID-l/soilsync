f2f0614e2dba749f672412e30dbd011a1a67f889
CUPID <archith837@gmail.com>
2025-04-20 01:34:35 +0530

    autopgreportfix1

--- lib/prediction.ts
+++ lib/prediction.ts
@@ -9,13 +9,43 @@ interface SoilData {
 }
 
 export async function getPrediction(soilData: SoilData): Promise<string> {
+  // Format the data according to the API requirements
+  const formattedData = {
+    topsoil: [
+      soilData.temperature,
+      soilData.moisture,
+      soilData.ph,
+      soilData.nitrogen,
+      soilData.phosphorus,
+      soilData.potassium
+    ],
+    subsoil: [
+      soilData.temperature,
+      soilData.moisture,
+      soilData.ph,
+      soilData.nitrogen,
+      soilData.phosphorus,
+      soilData.potassium
+    ],
+    deepsoil: [
+      soilData.temperature,
+      soilData.moisture,
+      soilData.ph,
+      soilData.nitrogen,
+      soilData.phosphorus,
+      soilData.potassium
+    ],
+    soil_type: 0, // Default to Clayey soil
+    crop_type: 0  // Default to Coconut
+  }
+
   const response = await fetch(process.env.NEXT_PUBLIC_HF_API_URL!, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_HF_API_TOKEN}`
     },
-    body: JSON.stringify(soilData)
+    body: JSON.stringify(formattedData)
   })
 
   if (!response.ok) {
